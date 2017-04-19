b3bed7252cb84eea6bbf2449d53605cac5103d42
Kerry Wilson <kerry.wilson@premisehealth.com>
2017-04-19 18:03:27 -0500

    plan route and import fix

---
+++ routes/plan.go
@@ -0,0 +1,36 @@
+package routes
+
+import (
+	"net/http"
+	"path"
+
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/webdevwilson/terraform-ci/config"
+	"github.com/webdevwilson/terraform-ci/model"
+)
+
+func init() {
+	r := Router()
+	r.HandleFunc("/api/plan/{guid}", wrapHandler(planGet)).Methods("GET")
+}
+
+func planGet(req *http.Request) (data interface{}, err error) {
+	guid := mux.Vars(req)["guid"]
+	dir := config.Get().CheckoutDirectory
+
+	project, err := model.GetProject(guid)
+	if err != nil {
+		return
+	}
+
+	plan, err := os.Open(path.Join(dir, project.Name, "terraform.tfplan"))
+	if err != nil {
+		return
+	}
+
+	data, err = terraform.ReadPlan(plan)
+	return
+}
